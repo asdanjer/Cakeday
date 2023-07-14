98601cec09288096d982e5b097be65de1cf0f8ab
asdanjer <asdanjer@web.de>
2023-07-14 17:09:47 +0200

    lets a go

---
+++ src/main/java/me/asdanjer/cakeday/Cakeday.java
@@ -0,0 +1,17 @@
+package me.asdanjer.cakeday;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cakeday extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
